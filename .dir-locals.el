3be50b4db4808765f748187ebacb1774abcd77ed
Ryan Prior <ryanprior@gmail.com>
2015-11-11 20:05:29 -0600

    Specifies c++11 standard for Emacs flycheck mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,10 +2,11 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((c++-mode
+  (flycheck-clang-language-standard . "c++11")
+  (flycheck-gcc-language-standard . "c++11")
   (eval setq-local cd-compile-directory
         (file-name-directory
          (car
           (dir-locals-find-file ".")))))
  (c-mode
   (mode . c++)))
-
