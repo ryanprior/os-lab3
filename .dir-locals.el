4cf20af87800a5e9f0f195857d2786b169b6f11d
Ryan Prior <ryanprior@gmail.com>
2015-11-14 20:21:38 -0600

    Updates Emacs cd-compile-directory

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,12 +1,14 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c++-mode
+((prog-mode
   (flycheck-clang-language-standard . "c++11")
   (flycheck-gcc-language-standard . "c++11")
-  (eval setq-local cd-compile-directory
-        (file-name-directory
-         (car
-          (dir-locals-find-file ".")))))
+  (eval . (setq-local cd-compile-directory
+                      (file-name-directory
+                       (concat
+                        (car
+                         (dir-locals-find-file "."))
+                        "build/")))))
  (c-mode
   (mode . c++)))
