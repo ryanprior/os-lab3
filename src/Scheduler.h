12ea82e488c445f0d3164219505c25aae180817f
Ryan Prior <ryanprior@gmail.com>
2015-11-24 00:15:09 -0600

    Adds more time quantum logic to Scheduler

--- src/Scheduler.h
+++ src/Scheduler.h
@@ -16,19 +16,41 @@ public:
                                      // expires.
   virtual ~Scheduler() {}
   virtual void Add(uint cpu_time, process_T *proc) = 0;
-  virtual uint NextEventTime(uint cpu_time) = 0;
+  virtual uint NextEventTime(uint cpu_time) const = 0;
   virtual void DispatchEvent(uint cpu_time) = 0;
+  virtual void AdvanceTime(uint old_time, uint new_time) = 0;
+  virtual bool empty() const = 0;
+  virtual void handle_proc_stop(process_T *proc) = 0;
 protected:
-  explicit Scheduler() {}
+  explicit Scheduler()
+    : m_proc_running(false)
+  {}
+  bool m_proc_running;
 };
 
 
+template <class process_T>
 class SchedulerTimeQuantum {
 protected:
   SchedulerTimeQuantum(uint time_q)
-    : m_time_quantum(time_q)
+    : m_time_quantum(time_q),
+      m_tq_remaining(time_q)
   {}
-  uint m_time_quantum;
+  virtual void handle_proc_tq_expire(process_T *proc) = 0;
+  const uint m_time_quantum;
+  uint m_tq_remaining;
+};
+
+
+template <class process_T>
+class SchedulerAge {
+protected:
+  SchedulerAge(uint age_time)
+    : m_age_time(age_time)
+  {}
+  virtual process_T* oldest_proc() const = 0;
+  virtual void age_process(process_T *proc, uint cpu_time) = 0;
+  const uint m_age_time;
 };
 
 #endif
